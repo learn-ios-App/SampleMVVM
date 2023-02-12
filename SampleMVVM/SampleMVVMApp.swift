9483885698645d64d50bbfbe0631657ddfc18821
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-12 17:39:32 +0900

    Initial Commit

---
+++ SampleMVVM/SampleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMVVMApp.swift
+//  SampleMVVM
+//
+//  Created by 渡邊魁優 on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct SampleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
